ac0d1d25372f1492100e386c0f643dce1be9834b
symonsajib <symon.sajib@gmail.com>
2019-11-27 19:57:13 -0500

    Add files via upload

---
+++ ex3_drill.py
@@ -0,0 +1,25 @@
+#counting my eggs
+print ("I will now count my chicken: ")
+#number of hens
+print("Hens", 25.0 + 30.0 / 6.0)
+#number of roosters
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+#egg counting
+print("Now I will count the eggs: ")
+#egg number
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+#comparing statement by string
+print("Is it true that 3.0 + 2.0 < 5.0 - 7.0?")
+#comparison
+print(3.0 + 2.0 < 5.0 - 7.0)
+#combining comparison statement and the comparison itself
+print("What is 3.0 + 2.0?", 3.0 + 2.0)
+print("What is 5.0 - 7.0?", 5.0 - 7.0)
+#printing reason
+print("oh, that's why it's false.")
+#printing string
+print("How about some more.")
+# some more comparison
+print("Is it greater?", 5.0 > -2.0)
+print("Is it gteater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
